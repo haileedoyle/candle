e680dd6280e67d47b0b093e2cb134bfc0d6f217a
Hailee Doyle <haileexdoyle@gmail.com>
2020-11-11 18:57:16 -0700

    displays on localhost

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 // import logo from './logo.svg';
-import React from "react";
+import React from 'react';
 import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from "./Home";
 
 function App() {
   return (
     <div className="App">
-      <h1>Candle</h1>
+      <Router>
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
